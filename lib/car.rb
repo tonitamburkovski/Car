26dcb4ec135c45c7583dafba79df9afaf5726a2d
tonitamburkovski <60368557+tonitamburkovski@users.noreply.github.com>
2020-03-07 12:10:21 +0100

    Update car.rb

--- lib/car.rb
+++ lib/car.rb
@@ -1,6 +1,36 @@
-require "car/version"
-
 module Car
-  class Error < StandardError; end
-  # Your code goes here...
+  class MyCar
+    attr_accessor :color, :current_speed
+    attr_reader :year, :defects
+    SPEED_OFFSET = 15
+
+    def initialize(year, color, model)
+      @year = year
+      @color = color
+      @model = model
+      @current_speed = 0
+      @defects = []
+    end
+
+    def speed_up(inc = SPEED_OFFSET)
+      @current_speed += inc
+    end
+
+    def slow_down(dec = SPEED_OFFSET)
+      @current_speed -= dec
+    end
+
+    def spray_paint(paint)
+      @color = paint
+    end
+
+    def add_defect(defect)
+      @defects.push(defect)
+    end
+
+    def service_info
+      defects = @defects.join(', ') 
+      "Defects to be fixed: #{defects}"
+    end
+  end
 end
